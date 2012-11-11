9a2274cd3eecab1a528d028674561b3148d02140
WolfgangMarx <information@wolfgangmarx.net>
2012-11-11 16:29:22 +0100

    First push containing ideas for the shopping list.
    These ideas require cleanups.

---
+++ microKitchen/Helper.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.SPOT;
+using Gadgeteer.Modules.GHIElectronics;
+using System.Threading;
+using GHIElectronics.NETMF.Glide.Display;
+using GHIElectronics.NETMF.Glide.UI;
+using GHIElectronics.NETMF.Glide;
+
+namespace microKitchen
+{
+
+    static class Helper
+    {
+
+        public static string GetStringValue(this ShoppingItemTypes type) {
+            switch (type)
+            {
+                case ShoppingItemTypes.Dairy:
+                    return "Dairy";
+                case ShoppingItemTypes.Frozen:
+                    return "Frozen";
+                case ShoppingItemTypes.Meat_SeaFood:
+                    return "Meat_SeaFood";
+                case ShoppingItemTypes.UnCategorised:
+                    return "UnCategorised";
+            }
+            return string.Empty;
+        }
+
+        public static ShoppingItemTypes ToShoppingItemTypes(this string value)
+        {
+            switch (value)
+            {
+                case "Dairy": return ShoppingItemTypes.Dairy;
+                case "Frozen": return ShoppingItemTypes.Frozen;
+                case "Meat_SeaFood": return ShoppingItemTypes.Meat_SeaFood;
+                case "UnCategorised": return ShoppingItemTypes.UnCategorised;
+            }
+            return ShoppingItemTypes.UnCategorised;
+        }
+
+        #region "Keyboard"
+
+        public static Keyboard InitKeyboard()
+        {
+            Keyboard keyboard = new Keyboard(320, 128, 3, 32, 0);
+
+            // Each view with keys in a up position.
+            keyboard.BitmapUp = new Bitmap[4]
+            {
+                Resources.GetBitmap(Resources.BitmapResources.Keyboard_320x128_Uppercase),
+                Resources.GetBitmap(Resources.BitmapResources.Keyboard_320x128_Lowercase),
+                Resources.GetBitmap(Resources.BitmapResources.Keyboard_320x128_Numbers),
+                Resources.GetBitmap(Resources.BitmapResources.Keyboard_320x128_Symbols)
+            };
+
+            // We must set the default key content.
+
+            string[][] keyContent = new string[4][];
+
+            // Letters
+            keyContent[0] = new string[10] { "q", "w", "e", "r", "t", "y", "u", "i", "o", "p" };
+            keyContent[1] = new string[9] { "a", "s", "d", "f", "g", "h", "j", "k", "l" };
+            keyContent[2] = new string[9] { Keyboard.ActionKey.ToggleCase, "z", "x", "c", "v", "b", "n", "m", Keyboard.ActionKey.Backspace };
+            keyContent[3] = new string[5] { Keyboard.ActionKey.ToNumbers, ",", Keyboard.ActionKey.Space, ".", Keyboard.ActionKey.Return };
+            keyboard.SetViewKeyContent(Keyboard.View.Letters, keyContent);
+
+            // Numbers
+            keyContent[0] = new string[10] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+            keyContent[1] = new string[10] { "@", "#", "$", "%", "&", "*", "-", "+", "(", ")" };
+            keyContent[2] = new string[9] { Keyboard.ActionKey.ToSymbols, "!", "\"", "'", ":", ";", "/", "?", Keyboard.ActionKey.Backspace };
+            keyContent[3] = new string[5] { Keyboard.ActionKey.ToLetters, ",", Keyboard.ActionKey.Space, ".", Keyboard.ActionKey.Return };
+            keyboard.SetViewKeyContent(Keyboard.View.Numbers, keyContent);
+
+            // Symbols
+            keyContent[0] = new string[10] { "~", "`", "|", "•", "√", "π", "÷", "×", "{", "}" };
+            keyContent[1] = new string[10] { Keyboard.ActionKey.Tab, "£", "¢", "€", "º", "^", "_", "=", "[", "]" };
+            keyContent[2] = new string[9] { Keyboard.ActionKey.ToNumbers, "™", "®", "©", "¶", "\\", "<", ">", Keyboard.ActionKey.Backspace };
+            keyContent[3] = new string[5] { Keyboard.ActionKey.ToLetters, ",", Keyboard.ActionKey.Space, ".", Keyboard.ActionKey.Return };
+            keyboard.SetViewKeyContent(Keyboard.View.Symbols, keyContent);
+
+            // or we could just call this:
+            // keyboard.DefaultKeyContent();
+
+            int[][] keyWidth = new int[4][];
+
+            // Each array entry represents a row of keys on the keyboard top-down (0-3)
+            // Each array within these entries contains the widths of the keys for that row.
+            // For example: keyWidth[0] = new int[10] { 48, 48, 48, 48, 48, 48, 48, 48, 48, 48 }
+            // represents the first row (0) which contains the keys Q, W, E, R, T, Y, U, I, O, P
+
+            // Letters
+            keyWidth[0] = new int[10] { 32, 32, 32, 32, 32, 32, 32, 32, 32, 32 };
+            keyWidth[1] = new int[9] { 32, 32, 32, 32, 32, 32, 32, 32, 32 };
+            keyWidth[2] = new int[9] { 48, 32, 32, 32, 32, 32, 32, 32, 48 };
+            keyWidth[3] = new int[5] { 48, 32, 160, 32, 48 };
+
+            keyboard.SetViewKeyWidth(Keyboard.View.Letters, keyWidth);
+
+            // Numbers
+            keyWidth[0] = new int[10] { 32, 32, 32, 32, 32, 32, 32, 32, 32, 32 };
+            keyWidth[1] = new int[10] { 32, 32, 32, 32, 32, 32, 32, 32, 32, 32 };
+            keyWidth[2] = new int[9] { 48, 32, 32, 32, 32, 32, 32, 32, 48 };
+            keyWidth[3] = new int[5] { 48, 32, 160, 32, 48 };
+
+            keyboard.SetViewKeyWidth(Keyboard.View.Numbers, keyWidth);
+
+            // Symbols
+            keyWidth[0] = new int[10] { 32, 32, 32, 32, 32, 32, 32, 32, 32, 32 };
+            keyWidth[1] = new int[10] { 32, 32, 32, 32, 32, 32, 32, 32, 32, 32 };
+            keyWidth[2] = new int[9] { 48, 32, 32, 32, 32, 32, 32, 32, 48 };
+            keyWidth[3] = new int[5] { 48, 32, 160, 32, 48 };
+
+            keyboard.SetViewKeyWidth(Keyboard.View.Symbols, keyWidth);
+
+            keyboard.CalculateKeys();
+            return keyboard;
+        }
+
+        #endregion
+
+
+    }
+
+}
---
+++ microKitchen/ShoppingList.cs
@@ -0,0 +1,83 @@
+using System;
+using Microsoft.SPOT;
+using System.Collections;
+using GHIElectronics.NETMF.Glide.UI;
+
+namespace microKitchen
+{
+
+    enum ShoppingItemTypes
+    {
+        Meat_SeaFood,
+        Dairy,
+        Frozen,
+        UnCategorised
+    }
+
+    class ShoppingList
+    {
+        public ArrayList Items { get; set; }
+
+        public ShoppingList()
+        {
+            this.Items = new ArrayList();
+        }
+
+        public ShoppingListItem GetItem(string name)
+        {
+            for (int i = 0; i < this.Items.Count; i++)
+            {
+                if (name.Equals(((ShoppingListItem)this.Items[i]).Name))
+                {
+                    return (ShoppingListItem)this.Items[i];
+                }
+            }
+            return null;
+        }
+
+        public ShoppingListItem GetItem(int position)
+        {
+            return (ShoppingListItem)Items[position];
+        }
+
+        public int Length()
+        {
+            return this.Items.Count;
+        }
+
+        public void Add(ShoppingListItem item)
+        {
+            this.Items.Add(item);
+        }
+
+        public void Add(string name, ShoppingItemTypes type, int numberOfItem)
+        {
+            this.Items.Add(new ShoppingListItem(name, type, numberOfItem));
+        }
+
+        public void Remove(int position)
+        {
+            this.Items.Remove(this.Items[position]);
+        }
+
+        public void Remove(ShoppingListItem item)
+        {
+            this.Items.Remove(item);
+        }
+    
+    }
+
+    class ShoppingListItem
+    {
+        public string Name { get; set; }
+        public ShoppingItemTypes Type { get; set; }
+        public int NumberOfItems { get; set; }
+
+        public ShoppingListItem(string name, ShoppingItemTypes type, int number)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.NumberOfItems = number;
+        }
+    }
+}
